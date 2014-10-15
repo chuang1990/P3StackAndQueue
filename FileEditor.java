e7aff554d775f8680a4d89bf48fe09d86f43f4bc
chuang1990 <animaniac1911@gmail.com>
2014-10-15 16:06:24 -0400

    fixed the errors on the initial commit, haven't test anything yet.

--- FileEditor.java
+++ FileEditor.java
@@ -1,48 +1,56 @@
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileNoFoundException;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 /**
  * FileEditor class
- **/
- 
-public class FileEditor{
+ *
+ */
+public final class FileEditor {
+
     // instance variables
+
     private File file;
     private Scanner scanText;
     private FileReader readText;
-    private String[] text;
-    
-    public FileEditor(File file){
-      fileReader();
+    private final ArrayList<String> text;
+
+    public FileEditor(File file) {
+        text = new ArrayList<>();
+        fileReader();
     }
-    
+
     /**
      * read from the textFile
-     **/
-    public void fileReader(){
+        *
+     */
+    public void fileReader() {
         try {
-        //set up variables
-        file = new File("hiker.txt");
-        readText = new FileReader(file);
-        scanText = new Scanner(readText);
-        
-        while(scanText.hasNextLine()){
-          String info = scan.nextLine();
-          if(!text.isEmpty()){
-            text.add(info);
-          }
+            //set up variables
+            file = new File("hiker.txt");
+            readText = new FileReader(file);
+            scanText = new Scanner(readText);
+
+            while (scanText.hasNextLine()) {
+                String info = scanText.nextLine();
+                if (!info.isEmpty()) {
+                    text.add(info);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
-        
-      }
-      catch(FileNotFoundException e) {
-          System.out.println("File not found");
-      }
-}
-/**
- * write hiker info into text file
- **/
-public void fileWriter(){
-    //need add more
+    }
+
+    /**
+     * write hiker info into text file
+ *
+     */
+    public void fileWriter() {
+        //need add more
+    }
 }
