94e4d788e8d2c32e1e525b705a433f4ab5107a8a
Cathilufia <c_huang4@post.massbay.edu>
2014-10-16 09:29:32 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,15 +1,18 @@
-//Java
+//Action
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 //Swing
-import javax.swing.JButton;
+import javax.swing.*;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EtchedBorder;
 
+//experiment
 //JavaFX
-import javafx.scene.text.Text;
+//import javafx.scene.text.Text;
 
 
 /**
@@ -20,12 +23,17 @@ public class GUI{
     //instance variables
     private JFrame frame;
     private JPanel mainPanel;
+    //buttons
     private JPanel btnPanel;
     private JButton startBtn;
     private JButton checkBtn;
     private JButton openBtn;
-    private JTextField textArea;
-    private final TrailEntrance trails;
+    //text area
+    private JPanel textPanel;
+    private JTextArea textArea;
+    private JScrollPane textScroller;
+    //other variables
+    private static TrailEntrance trails;
     private FileEditor fileEditor;
     
     /**
@@ -56,17 +64,22 @@ public class GUI{
     public void setContent(JFrame frame){
         Container contentPane = frame.getContentPane();
         
+        makeButton();
+        makeTextArea();
+        makeLabels();
         
+        contentPane.add(btnPanel, BorderLayout.SOUTH);
+        contentPane.add(textPanel, BorderLayout.NORTH);
     }
     
     /**
      * create the buttons
      **/
     public void makeButton(){
-        btnPanel = new JPanel(new BorderLayout());
+        btnPanel = new JPanel(new GridLayout(1,3));
         
-        JLabel label = new JLabel("Purple Mountain Trail");
-        label.setFont(new Font("Serif", Font.BOLD+ITALIC, 20));
+        //JLabel label = new JLabel("Purple Mountain Trail");
+        //label.setFont(new Font("Serif", Font.BOLD+ITALIC, 20));
         
         startBtn = new JButton("Start");
         startBtn.addActionListener(new ActionListener(){
@@ -99,13 +112,30 @@ public class GUI{
             
             }
         });
+        
+        //btnPanel.add(label, BorderLayout.NORTH);
+        btnPanel.add(startBtn);
+        btnPanel.add(openBtn);
+        btnPanel.add(checkBtn);
     }
     
     /**
      * create the textArea
      **/
     public void makeTextArea(){
-        textArea = new JTextField();
+        textPanel = new JPanel();
+        textPanel.setBorder(new EtchedBorder(10,Color.BLACK, Color.GRAY));
+        
+        textArea = new JTextArea(30, 30);
+        //set textArea to non-editable
+        textArea.setEditable(false);
+        
+        //create a scroller for the textArea
+        textScroller = new JScrollPane(textArea);
+        //set scroller to always be vertical
+        textScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);;
+        
+        textPanel.add(textScroller);
     }
     
     /**
