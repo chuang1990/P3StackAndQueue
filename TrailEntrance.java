b305963b492002059a0b6751af418d1bbada2dc7
Cathilufia <c_huang4@post.massbay.edu>
2014-10-16 08:34:59 -0400

    added random number generators

--- TrailEntrance.java
+++ TrailEntrance.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Random;
 
 /**
  * TrailEntrance class
@@ -12,13 +13,18 @@ public class TrailEntrance{
     private LinkedList<Group> trail4;
     private int trailNum;
     private int hikerNum;
+    private Random randomTrail;
+    private Random randomHikers;
     
     /**
      * constructor
      **/
     public TrailEntrance(){
-        //initialize variables
-        queue = new LinkedList();
+        //initialize queues
+        trail1 = new LinkedList();
+        trail2 = new LinkedList();
+        trail3 = new LinkedList();
+        trail4 = new LinkedList();
     }
     
     /**
@@ -34,11 +40,34 @@ public class TrailEntrance{
     //    return full;
     //}
     
+    
     /**
      * add groups of hiker to the queue
      **/
-    public void addGroupToQ(Group group){
+    public void addGroupToQ(int trail, Group group){
          //need more code
+    }
+    
+    /**
+     * Generate a random trail number for the group to go into
+     * 
+     * @return int
+     **/
+    public int trailNumbGenerator(){
+        randomTrail = new Random();
+        trailNum = randomTrail.nextInt(5)+1;
+        return trailNum;
+    }
+    
+    /**
+     * Generate a random number of hikers.
+     * 
+     * @return int
+     **/
+    public int hikerGenerator(){
+        randomHikers = new Random();
+        hikerNum = randomHikers.nextInt(201)+1;
+        return hikerNum;
     }
     
     /**
@@ -66,3 +95,4 @@ public class TrailEntrance{
         //need more work
         return "";
     }
+}
