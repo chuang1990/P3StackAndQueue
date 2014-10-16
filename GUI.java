9e7d6e455b88f09a1b8b09bf148f917caa8f2a21
Cathilufia <c_huang4@post.massbay.edu>
2014-10-15 22:58:21 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -1,8 +1,15 @@
+//Java
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+//Swing
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+//JavaFX
+import javafx.scene.text.Text;
 
 
 /**
@@ -12,6 +19,8 @@ public class GUI{
 
     //instance variables
     private JFrame frame;
+    private JPanel mainPanel;
+    private JPanel btnPanel;
     private JButton startBtn;
     private JButton checkBtn;
     private JButton openBtn;
@@ -24,49 +33,69 @@ public class GUI{
      **/
     public GUI(){
         trails = new TrailEntrance();
+        this.makeWindow();
     }
     
     /**
      * create the frame
      **/
-    public void makeWindeow(){
+    public void makeWindow(){
         frame = new JFrame("Purple Mountain Trail");
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        setContent(frame);
         
+        frame.pack();
+        frame.setVisible(true);
     }
     
     /**
      * put the contents together in the container
      **/
     public void setContent(JFrame frame){
-    
+        Container contentPane = frame.getContentPane();
+        
+        
     }
     
     /**
      * create the buttons
      **/
     public void makeButton(){
+        btnPanel = new JPanel(new BorderLayout());
+        
+        JLabel label = new JLabel("Purple Mountain Trail");
+        label.setFont(new Font("Serif", Font.BOLD+ITALIC, 20));
+        
         startBtn = new JButton("Start");
         startBtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-            
+                //initialize the program
+                trails = new TrailEntrance();
             }
         });
         
         openBtn = new JButton("Open");
         openBtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                
+                //clear the stacks that has more than 10 hikers from the all the queues.
+                //prints out the info from the released stacks
+                //writes to file
+                //throw exemption if startBtn hasn't been pressed
             }
         });
         
-        checkBtn = new JButton("Open");
+        checkBtn = new JButton("Check");
         checkBtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
+                //empty the queues.
+                //print out the info of all the stacks that got cleared
+                //writes to file
+                //throw exemption if startBtn hasn't been pressed
+                
             
             }
         });
