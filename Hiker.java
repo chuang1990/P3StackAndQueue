49fdb30c5262325c356be5579fe2906813c92e64
hriggs <hkriggs128@gmail.com>
2014-10-17 16:39:50 -0400

    Update Hiker.java

--- Hiker.java
+++ Hiker.java
@@ -1,54 +1,50 @@
 /**
  * Hiker class
- * 
+ *
  * The Hiker Class is part of the implementation of "Hiker Program"
- * 
- * The Hiker Class is in charge to create a Hiker Object which has a hikerId 
- * This class provide the setters and getters for the HikerId and also the toString method 
+ *
+ * The Hiker Class is in charge to create a Hiker Object which has a hikerId
+ * This class provide the setters and getters for the HikerId and also the toString method
  * that prints the state of the object
  * 
+ * @author (your name) 
+ * @version 1/17/14
  **/
 public class Hiker
 {
-
-    //instance variables
+    // id of hiker as String (HikerNum) 
     private String hikerId;
     
-    /**
-     * constructor
-     *
-     * @param hikerid String
-     */
-    public Hiker(String hikerid)
-    {
-        this.hikerId = hikerid;
-        
-    }
+    // id number that is incremented for each hiker
+    private static int idNum = 0; 
     
     /**
-     * set hiker's id
+     * Constructor for Hiker using class variable idNum that increments with each new Hiker created. 
+     */
+    public Hiker()
+    {
+        idNum++; 
+        hikerId = "Hiker" + idNum; 
+    }
+
+    /**
+     * Gets the hiker's id string.
+     * Represented as HikerNum (Hiker1, Hiker2, etc). 
      * 
-     * @return name String
-     */
-    public void setHikerId(String name)
-    {
-        this.hikerId = name;
-    }
-    
-    /**
-     * get the hiker's id
+     * @return     the hiker's id
      */
     public String getHikerId()
     {
         return hikerId;
     }
-    
+
     /**
-     * returns the state of the object in a string
+     * Returns the hiker's info.
+     * 
+     * @return    string representation of hiker
      */
     public String toString()
     {
         return ("Hiker info: " + hikerId);
     }
-     
 }
