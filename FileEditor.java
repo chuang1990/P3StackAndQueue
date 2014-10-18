360727b629fe396e72410cb2a11e7de34fef9862
Cathilufia <c_huang4@post.massbay.edu>
2014-10-18 11:27:53 -0400

    Update FileEditor.java

--- FileEditor.java
+++ FileEditor.java
@@ -1,10 +1,6 @@
 
 import java.util.Scanner;
 import java.io.*;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 /**
  * FileEditor class
@@ -17,55 +13,29 @@ public final class FileEditor {
     private File file;
     private Scanner scanText;
     private FileReader readText;
-    private final ArrayList<String> text;
     private Writer writer;
 
-    public FileEditor(File file) {
-        text = new ArrayList<>();
-        fileReader();
+    public FileEditor(String s){
+        fileWriter(s);
     }
 
+   
     /**
-     * read from the textFile
-        *
+     * Create a file and write hiker info into it.
      */
-    public void fileReader() {
-        try {
-            //set up variables
-            file = new File("hiker.txt");
-            readText = new FileReader(file);
-            scanText = new Scanner(readText);
-
-            while (scanText.hasNextLine()) {
-                String info = scanText.nextLine();
-                if (!info.isEmpty()) {
-                    text.add(info);
-                }
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-    }
-
-    /**
-     * write hiker info into text file
- *
-     */
-    public void fileWriter() {
+    public void fileWriter(String s) {
         //need add more
          try{
             //store the time that the file is created into a string
             time = new Date();
-            timeStamp = time.toString();
+            String timeStamp = time.toString();
             //create new file
             //file = new File("Hiker"+timeStamp+".txt");
-            //creates a writer that uses buffered Writer to writer to the newly created file.
-            //timeStamp prevent files to overwrite each other.
             writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream("Hiker"+timeStamp+".txt"), "utf-8"));
-                //create a loop that takes in all the created hikers and write them in.
-                writer.write("Something");
+                
+                writer.write(s);
+                
             } catch (IOException ex) {
                 // report
             } finally {
