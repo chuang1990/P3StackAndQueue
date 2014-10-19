73c89644c5d2b2fe49149931807e4cb1a857f0bd
mmar14 <marmoncaleano@gmail.com>
2014-10-18 21:51:24 -0400

    Update FileEditor.java

--- FileEditor.java
+++ FileEditor.java
@@ -1,49 +1,58 @@
 
+import java.util.Date;
 import java.util.Scanner;
 import java.io.*;
-import java.util.Date;
-
 /**
- * FileEditor class
+ * The FileEditor class
  * 
- * @Catherine
- * @version 10/18/2014
+ * The Class FileEditor is in charge of the interations with the file. 
+ * It has methods that allow it to write in to a file
+ * 
+ * @author  (Catherine Huang, Hannah Riggs, Maria del Mar Moncaleano) 
+ * @version 1/17/14
  */
-public final class FileEditor {
-
-    // instance variables
-
+public final class FileEditor 
+{
     private File file;
     private Scanner scanText;
     private FileReader readText;
+    private Writer writer;
     private Date time;
-    private Writer writer = null;
 
-    public FileEditor(String s){
+    public FileEditor(String s)
+    {
         fileWriter(s);
     }
 
-   
     /**
      * Create a file and write hiker info into it.
      */
-    public void fileWriter(String s) {
-        //need add more
-         try{
+    public void fileWriter(String s) 
+    {
+        try
+        {
             //store the time that the file is created into a string
             time = new Date();
             String timeStamp = time.toString();
-            //create new file
-            //file = new File("Hiker"+timeStamp+".txt");
+            //creates a writer that uses buffered Writer to writer to the newly created file.
+            //timeStamp prevent files to overwrite each other.
             writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("Hiker"+timeStamp+".txt"), "utf-8"));
-                
-                writer.write(s);
-                
-            } catch (IOException ex) {
-                // report
-            } finally {
-                try {writer.close();} catch (Exception ex) {}
-            }
+                    new FileOutputStream("Hiker"+timeStamp+".txt"), "utf-8"));
+            //create a loop that takes in all the created hikers and write them in.
+            writer.write(s);
+
+        } 
+        catch (IOException ex) 
+        {
+            // report
+        } 
+        finally 
+        {
+            try
+            {
+                writer.close();
+            } 
+            catch (Exception ex) {}
+        }
     }
 }
