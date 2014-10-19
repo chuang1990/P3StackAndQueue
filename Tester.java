03b722556bbd9f0a940643729dbd5ec7fd254103
mmar14 <marmoncaleano@gmail.com>
2014-10-18 21:50:36 -0400

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,12 +1,15 @@
 /**
- * Tester class
+ * TheTester class
+ * 
+ * TheTester class has a main method that runs the program. This allows the user to test the program
+ * @author (Catherine Huang, Hannah Riggs, Maria del Mar Moncaleano) 
+ * @version 10/18/2014
  **/
 
-public class Tester{
-
-    
-
-    public static void main(String argc[]){
+public class Tester
+{
+    public static void main(String argc[])
+    {
         GUI gui = new GUI();
     }
 }
