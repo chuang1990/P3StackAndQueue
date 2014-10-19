581b22e14b3986fb56ded121064d1bdefcac8912
mmar14 <marmoncaleano@gmail.com>
2014-10-18 21:49:13 -0400

    Update TrailEntrance.java

--- TrailEntrance.java
+++ TrailEntrance.java
@@ -2,19 +2,25 @@ import java.util.*;
 /**
  * TrailEntrance class
  * 
- * @author (your name) 
- * @version 1/17/14
+ * The TrailEntrance Class acts as te Queue data structure that holds Group objects. 
+ * The linkedLIst is used as the queue as it extends the queue interface and can behave as a queue.
+ * Each trailEntrance has a number. It has methods that can add and remove Group objects to the queue,
+ * get the TrailEntrance number and get the information about the trail entrance.
+ * 
+ * @author (Catherine Huang, Hannah Riggs, Maria del Mar Moncaleano) 
+ * @version 10/18/2014
  **/
-public class TrailEntrance {
+public class TrailEntrance 
+{
     // the linked list will be treated as a queue
     private Queue<Group> trail;
     // number of groups in the queue
     private int numberGroups; 
-
     /**
      * Constructor of TrailEntrance object. 
      **/
-    public TrailEntrance(){
+    public TrailEntrance()
+    {
         trail = new LinkedList();
         numberGroups = 0; 
     }
@@ -40,7 +46,7 @@ public class TrailEntrance {
         numberGroups--;
         return trail.remove(); 
     }
-    
+
     /**
      * Returns (but does not remove) first group in queue. 
      * 
@@ -50,7 +56,7 @@ public class TrailEntrance {
     {
         return trail.peek(); 
     }
-    
+
     /**
      * Returns the number of groups in the queue
      * 
